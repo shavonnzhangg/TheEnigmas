b4ace9064327a93a6dcee41a541b4c60b6314445
Ellie Rui <ellierui@Yilins-MacBook-Pro.local>
2018-03-04 13:49:04 -0800

    Google Oauth

--- TheEnigmas/TheEnigmas/AppDelegate.swift
+++ TheEnigmas/TheEnigmas/AppDelegate.swift
@@ -8,16 +8,22 @@
 
 import UIKit
 import Firebase
+import GoogleSignIn
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
 
     var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+    
         FirebaseApp.configure()
+        // Use Firebase library to configure APIs
+        
+        GIDSignIn.shareInstance().clientID = FirebaseApp.app()?.options.clientID
+        GIDSignIn.sharedInstance().delegate = self
         return true
     }
 
@@ -42,7 +48,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+    
+    @available(iOS 9.0, *)
+    func application(_application: UIApplication, open url : URL, options: [UIApplicationOpenURLOptionsKey : Any])
+        -> Bool{
+            return GIDSignIn.sharedInstance().handle(url,
+                                                     sourceApplication:options[UIApplicationOpenURLOptionsKey.sourceApplication] as? String,
+                                                     annotation : [:])
+        }
+    }
+    
+    func application(_ application: UIApplication, open url : URL, sourceApplicaiton : Stirng?, annotation: Any) -> Bool {
+        return
+    }
 
 
-}
+
+
 
