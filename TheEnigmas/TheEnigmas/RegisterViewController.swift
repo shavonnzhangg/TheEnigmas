30462dc0d63ba4828c93509e8661d9edd385882c
Ellie Rui <yilinrui@Yilins-MacBook-Pro.local>
2018-03-04 12:16:12 -0800

    registerViewController

---
+++ TheEnigmas/TheEnigmas/RegisterViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RegisterViewController.swift
+//  TheEnigmas
+//
+//  Created by Ellie Rui on 2018-03-04.
+//  Copyright © 2018 The Enigmas. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
